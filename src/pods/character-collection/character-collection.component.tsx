c1ee1d83c1166da3d69474e3e87b3b94968b2a5a
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-10-05 10:55:06 +0200

    adding material styles

--- src/pods/character-collection/character-collection.component.tsx
+++ src/pods/character-collection/character-collection.component.tsx
@@ -3,6 +3,16 @@ import { Link } from 'react-router-dom';
 import { CharacterVm } from './character-collection.vm';
 import { SearchFilterComponent } from 'common/components/search-filter.component';
 
+// Material UI
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+
 interface Props {
   characterCollection: CharacterVm[];
   handleOnSearch: (search: string) => Promise<void>;
@@ -17,7 +27,29 @@ export const CharacterCollectionComponent: React.FC<Props> = (props) => {
         handleOnSearch={handleOnSearch}
         placeholder="Search character name"
       />
-      <ul>
+      {!characterCollection.length && (
+        <p>No results were found for your search</p>
+      )}
+      <List>
+        {characterCollection.length > 0 &&
+          characterCollection.map((character) => (
+            <ListItem key={character.id}>
+              <ListItemAvatar>
+                <Avatar src={character.image} alt={character.name} />
+              </ListItemAvatar>
+              <ListItemText
+                primary={character.name}
+                secondary={character.species}
+              />
+              <ListItemSecondaryAction>
+                <Link to={`character/${character.id}`}>
+                  <ArrowForwardIcon />
+                </Link>
+              </ListItemSecondaryAction>
+            </ListItem>
+          ))}
+      </List>
+      {/* <ul>
         {!characterCollection.length && (
           <p>No results were found for your search</p>
         )}
@@ -36,7 +68,7 @@ export const CharacterCollectionComponent: React.FC<Props> = (props) => {
               <Link to={`character/${character.id}`}>Ver detalle</Link>
             </li>
           ))}
-      </ul>
+      </ul> */}
     </>
   );
 };
